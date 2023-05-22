2f758b973d0680d4437d6c5d4659214f79c8abf1
Hafizha Ulinnuha Ahmad <hafizhaua@gmail.com>
2023-05-22 21:09:51 +0700

    feat: fetch api in fe

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { useRef, useState } from "react";
 import DefaultLayout from "../components/DefaultLayout";
 import SearchBar from "../components/SearchBar";
 import { FaHatWizard, FaThumbsDown, FaThumbsUp } from "react-icons/fa";
+import axios from "axios";
 
 export default function Home() {
     const diseaseData = [
@@ -54,6 +55,7 @@ export default function Home() {
 
     const [selectedOptions, setSelectedOptions] = useState([]);
     const [isShowResult, setIsShowResult] = useState(false);
+    const [result, setResult] = useState({});
     const resultRef = useRef(null);
 
     const handleSubmit = (e) => {
@@ -61,6 +63,15 @@ export default function Home() {
 
         if (selectedOptions.length > 0) {
             console.log(selectedOptions);
+
+            axios
+                .post("http://127.0.0.1:5000/api", {
+                    symptoms: selectedOptions.map((l) => l.value),
+                })
+                .then((res) => {
+                    console.log(res.data);
+                    setResult(res.data);
+                });
 
             setIsShowResult(true);
 
@@ -106,7 +117,7 @@ export default function Home() {
                 </div>
             </div>
             <div
-                className={`bg-purple-500 border-t-2 min-h-screen border-purple-500 ${
+                className={`bg-purple-500 border-t-2 border-purple-500 ${
                     isShowResult ? "" : "hidden"
                 }`}
                 id="result"
@@ -117,46 +128,31 @@ export default function Home() {
                         Here are our diagnosis based on what you feel
                     </p>
 
-                    <div className="w-full bg-gradient-to-br from-purple-300/20 to-purple-900 min-h-screen my-8 md:py-12 md:px-16 py-4 px-8 rounded-xl">
-                        {diseaseData.map((data) => (
-                            <div className="border-b-[1px] border-b-purple-100/20 py-12">
-                                <h3 className="text-h3 font-bold mb-4">
-                                    {data.disease_name}
-                                    <span className="text-b-lg text-yellow-100 ml-2">
-                                        8.07%
-                                    </span>
-                                </h3>
-                                <div className="flex flex-col md:flex-row gap-4 items-center">
-                                    <div className="flex flex-wrap w-full md:w-5/12 gap-2 h-fit">
-                                        {data.symptoms.map((symptom, idx) => (
-                                            <div
-                                                className={`py-2 px-4 ${
-                                                    idx % 2 === 0
-                                                        ? "bg-purple-50"
-                                                        : "bg-purple-300"
-                                                } text-purple-900 rounded`}
-                                            >
-                                                {symptom}
-                                            </div>
-                                        ))}
-                                    </div>
-                                    <div className="flex md:w-7/12 gap-4 justify-center items-center w-full">
-                                        <button className="flex flex-col justify-center items-center gap-4 px-4 py-10 w-1/2 bg-purple-50 h-full group text-purple-900 rounded drop-shadow-xl">
-                                            <span className="text-h2 group-hover:-translate-y-1 transition">
-                                                <FaThumbsUp />
-                                            </span>
-                                            <p>Yes, this might be it</p>
-                                        </button>
-                                        <button className="flex flex-col group justify-center items-center gap-4 px-4 py-10 w-1/2 bg-purple-50 h-full text-purple-900 rounded drop-shadow-xl">
-                                            <span className="text-h2 group-hover:translate-y-1 transition">
-                                                <FaThumbsDown />
-                                            </span>
-                                            <p>No, I'm not looking for this</p>
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
+                    <div className="w-full bg-gradient-to-br from-purple-300/20 to-purple-900 my-8 md:py-12 md:px-16 py-4 px-8 rounded-xl">
+                        <div className=" py-12">
+                            <h3 className="text-h3 font-bold mb-4">
+                                {result.name}
+                                {/* <span className="text-b-lg text-yellow-100 ml-2">
+                                    8.07%
+                                </span> */}
+                            </h3>
+                            <p className="mb-4 font-thin">
+                                {result.description}
+                            </p>
+                            <p>Precautions:</p>
+                            <ul className="ml-4">
+                                {result.precautions?.map((precaution) => {
+                                    return (
+                                        <li
+                                            key={precaution}
+                                            className="list-disc"
+                                        >
+                                            {precaution}
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
