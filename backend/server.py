ef0ac25d962e581303ea7986ada464914da52a95
Hafizha Ulinnuha Ahmad <hafizhaua@gmail.com>
2023-06-05 11:33:49 +0700

    [feature] add article endpoint

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from disease import get_disease_info, generate_df
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS, cross_origin
 import pickle
 
@@ -10,9 +10,10 @@ CORS(app, supports_credentials=True)
 model = pickle.load(open("model.pkl", "rb"))
 
 
-@app.route("/api", methods=["POST", "GET"])
+@app.route("/api/prediction", methods=["POST", "GET"])
 @cross_origin(supports_credentials=True)
 def predict():
+    # try:
     data = request.get_json(force=True)
     x_pred = generate_df(data["symptoms"])
     prediction = model.predict(x_pred)
@@ -20,19 +21,46 @@ def predict():
     info = get_disease_info(output)
     return jsonify(
         {
-            "name": output,
-            "description": info["description"],
-            "precautions": info["precautions"],
-            "image": info["image"],
-            "doctors": info["doctors"],
-            "symptoms": info["symptoms"],
+            "status": "success",
+            "data": {
+                "name": output,
+                "description": info["description"],
+                "precautions": info["precautions"],
+                "image": info["image"],
+                "doctors": info["doctors"],
+                "symptoms": info["symptoms"],
+            },
         }
     )
+
+
+# except:
+#     return custom_error({"status": "error"}, 404)
+
+
+@app.route("/api/disease", methods=["POST", "GET"])
+@cross_origin(supports_credentials=True)
+def getData():
+    # try:
+    data = request.get_json(force=True)
+    disease = data["disease"]
+    info = get_disease_info(disease)
+    info["name"] = disease
+    print(info)
+    return jsonify({"status": "success", "data": info})
+
+
+# except:
+# return custom_error({"status": "error"}, 404)
 
 
 @app.route("/", methods=["GET"])
 def index():
     return "Machine Learning Inference"
+
+
+def custom_error(message, status_code):
+    return make_response(jsonify(message), status_code)
 
 
 if __name__ == "__main__":
